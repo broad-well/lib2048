6193f3bd9c423d151c7df9a83a6880be62d70ffa
Michael P <michael@mcmoo.org>
2017-04-07 21:03:10 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+
+// TypeScript
+const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
+const tsdoc = require('gulp-typedoc');
+
+// Optimization
+const uglify = require('gulp-uglify');
